a67d0417079db7ddf500f698eca72062cc7e359e
emersonnfs <rm95199@fiap.com.br>
2023-09-18 19:35:45 -0300

    Definido font do menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,19 +9,19 @@ const NavBar = ({ active }) => {
             <h1 className="text-3xl font-bold text-black-100">WatchList</h1>
           </a>
         </li>
-          <li>
-            <Link href="/" className={active == "home" && "text-slate-500   "}>
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/favoritos"
-              className={active == "favoritos" && "text-slate-300"}
-            >
-              Favoritos
-            </Link>
-          </li>
+        <li>
+          <Link href="/" className={active == "home"  ? "text-lg font-semibold text-black-200" : "text-black-400"}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/favoritos"
+            className={active == "favoritos"  ? "text-lg font-semibold text-black-200" : "text-black-400"}
+          >
+            Favoritos
+          </Link>
+        </li>
       </ul>
       <div className="h-12 w-12 rounded-full overflow-hidden">
         <img src="https://i.pravatar.cc/100" alt="avatar do usuário" />
